6cc7ab74da021bba6608f5328f2dc0dc0daf11fc
RomainBertrand <romaincmoi@laposte.net>
2020-01-03 10:01:48 +0100

    Rendu du code plus lisible forms et views

--- interface_recommandation/NetfleX/forms.py
+++ interface_recommandation/NetfleX/forms.py
@@ -1,7 +1,15 @@
+"""
+Classe NotationFilms, qui contient un champ pour le titre du film et un autre pour la note
+"""
+
 from django import forms
 
-import sqlite3
 
 class NotationFilms(forms.Form):
-    titre = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'list':'Films', 'class':'form_film'}))
+    """
+    le tire est un charfield pour que l'utilisateur puisse taper ce qu'il veut.
+    De plus, on passe la liste des titres de films en arguent du form html
+    """
+    titre = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'list': 'Films', 'class': 'form_film'}))
     note = forms.IntegerField(max_value=5, min_value=0)
--- interface_recommandation/NetfleX/views.py
+++ interface_recommandation/NetfleX/views.py
@@ -1,62 +1,75 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import NotationFilms 
+"""
+fichier qui gère l'affichage, créé les variables nécessaires pour les fichiers html.
+"""
+
 import sqlite3
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.forms import formset_factory
-from NetfleX.etude_donnees_base import *
-# Create your views here.
+from NetfleX.etude_donnees_base import meilleure_correlation
+from .forms import NotationFilms
+
 
 def manage_notations(request):
+    """ Gère le formulaire de demande de films et de notes """
+    # on créé la liste des noms de films pour avoir le menu déroulant
     lien = sqlite3.connect("base_noms_film.db")
     curseur = lien.cursor()
     curseur.execute("SELECT DISTINCT title FROM noms_film")
     liste_movies = curseur.fetchall()
-    i=0
+    i = 0
     for movie in liste_movies:
         liste_movies[i] = str(movie[0])
         i += 1
-
-    NotationFilmsFormSet = formset_factory(NotationFilms,extra=5,max_num=10)
-    films_choisis = []
+    # on créé une formSet factory issue de la classe NotationFilms
+    NotationFilmsFormSet = formset_factory(NotationFilms, extra=5, max_num=10)
+    films_choisis = []  # liste de titre et de notes correspondant aux films choisis
     if request.method == 'POST':
-        formset = NotationFilmsFormSet(request.POST,request.FILES)
+        formset = NotationFilmsFormSet(request.POST, request.FILES)
         if formset.is_valid():
             for film_form in formset:
                 titre = film_form.cleaned_data.get('titre')
                 note = film_form.cleaned_data.get('note')
-                films_choisis.append([titre,note])
-    else :
-        formset=NotationFilmsFormSet()
+                films_choisis.append([titre, note])
+    else:
+        formset = NotationFilmsFormSet()
     return render(request, "NetfleX/block_avis.html", locals())
 
+
 def home(request):
+    """ Page d'accueil """
     return HttpResponse("<h2>Hello World</h2>")
 
+
 def page_finale(request):
-    NotationFilmsFormSet = formset_factory(NotationFilms,extra=5,max_num=10)
+    """ Appelle la fonction de matching et affiche le résultat """
+    NotationFilmsFormSet = formset_factory(NotationFilms, extra=5, max_num=10)
     films_choisis = []
-    if request.method=="POST":
-        formset = NotationFilmsFormSet(request.POST,request.FILES)
+    if request.method == "POST":
+        formset = NotationFilmsFormSet(request.POST, request.FILES)
         if formset.is_valid():
             for film_form in formset:
                 titre = film_form.cleaned_data.get('titre')
                 note = film_form.cleaned_data.get('note')
-                films_choisis.append([titre,note])
+                films_choisis.append([titre, note])
             lien = sqlite3.connect("base_noms_film.db")
-            liste_titres=[]
+            liste_titres = []
             for film in films_choisis:
                 liste_titres.append(film[0])
             curseur = lien.cursor()
-            curseur.execute("SELECT movieId FROM noms_film WHERE title IN {}".format(str(tuple(liste_titres))))
+            # on récupère les Id des films choisis par l'utilisateur
+            curseur.execute("SELECT movieId FROM noms_film WHERE title IN {}".format(
+                str(tuple(liste_titres))))
             liste_movieId = curseur.fetchall()
             utilisateur = []
             for i in range(len(liste_movieId)):
-                utilisateur.append([int(liste_movieId[i][0]),films_choisis[i][1]])
+                utilisateur.append(
+                    [int(liste_movieId[i][0]), films_choisis[i][1]])
             film_recommande = [1]
+            # Meilleure corrélation en fonction des films et des notes de l'utilisateur
             film_recommande.append(str(meilleure_correlation(utilisateur)))
-            curseur.execute("SELECT title FROM noms_film WHERE movieId IN {}".format(str(tuple(film_recommande))))
+            curseur.execute("SELECT title FROM noms_film WHERE movieId IN {}".format(
+                str(tuple(film_recommande))))  # on récupère son titre
             titre_recommande = curseur.fetchall()[0][0]
             return render(request, "NetfleX/page_finale.html", locals())
-    else :
-        notation = NotationFilms() 
     return render(request, "NetfleX/block_avis.html", locals())
