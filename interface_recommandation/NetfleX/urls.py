13fb523cd53456b4562f5397d750f806c4c17e76
Maxime Barakat <maxime.barakat@eleves.enpc.fr>
2020-01-26 14:06:39 +0000

    url names

--- interface_recommandation/NetfleX/urls.py
+++ interface_recommandation/NetfleX/urls.py
@@ -4,12 +4,12 @@ from . import views
 # urlpattern is a Django variable, this is why it doesn't conform the snake_case convention
 
 urlpatterns = [
-    path('', views.home, name="accueil"),
-    path('recommendation/', views.manage_notations, name="Première page"),
-    path('final_page/', views.final_page, name="Page finale"),
-    path('advice/', views.advice, name='Conseil'),
-    path('contact/', views.contact, name="Page Contact"),
-    path('sources/', views.sources, name="Page Sources"),
+    path('', views.home, name="home"),
+    path('recommendation/', views.manage_notations, name="First" page"),
+    path('final_page/', views.final_page, name="Final page"),
+    path('advice/', views.advice, name='Advice'),
+    path('contact/', views.contact, name="Contact"),
+    path('sources/', views.sources, name="Sources"),
     path('changement/', views.change_movie_number,
-         name="Changement nombre movies"),
+         name="Change_movie_number"),
 ]
